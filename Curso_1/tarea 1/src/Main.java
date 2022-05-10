420d621f6f178b48d076dfd8b2da577730bff1a3
alejo95-svg <ing.alejandro_gaitan@hotmail.com>
2022-05-10 00:45:24 -0500

    tarea segunda parte

--- Curso_1/tarea 1/src/Main.java
+++ Curso_1/tarea 1/src/Main.java
@@ -5,9 +5,23 @@ public class Main {
         int c = 30;
         int total = suma(a,b,c);                               
         System.out.println("la suma es: "+ total);
+
+        coche miCoche = new coche();
+        miCoche.SumaPuertas();
+        System.out.println("las puertas del coche son: "+ miCoche.Npuertas);
+
     }
     
     public static int suma (int a, int b, int c){
         return a+b+c;
     }
+}
+
+class coche {
+    public int Npuertas = 0;
+
+    public void SumaPuertas (){
+        this.Npuertas++;
+    }
+
 }
